5f277ed9589e41e6e9e5899ef923530fab76f412
bmanek <41875460+bmanek@users.noreply.github.com>
2021-02-21 16:16:56 -0500

    add basic createUser route (#2)
    * add basic createUser route
    
    * Delete package-lock.json
    
    Delete unused file
    
    Co-authored-by: Eric Masi <47583837+git-masi@users.noreply.github.com>

---
+++ users-service/src/handlers/createUser.js
@@ -0,0 +1,22 @@
+// CREATE
+// There should be a route to create a user
+// The route should accept a unique displayName (there should be a reasonable limit to the length)
+// A user should be saved in the Users table
+// A new UUID should be generated
+// A created time should be added as an ISO string
+// A randomly generated amount of starting cash should be added (there should be a reasonable min/max)
+// A randomly generated number of starting stocks should be added (there should be a reasonable min/max)
+
+import { commonMiddleware } from '../lib/commonMiddleware';
+
+async function createUser(event, context) {
+  try {
+
+    return successResponse({ message: 'User created!' });
+  } catch (error) {
+    // console.log(error);
+    throw error;
+  }
+}
+
+export const handler = commonMiddleware(createUser);
