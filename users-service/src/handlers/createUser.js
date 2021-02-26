d03acf49ac5e42cad2bfec5031c69c138209d1e9
Eric Masi <47583837+git-masi@users.noreply.github.com>
2021-02-25 21:48:00 -0500

    Add users table (#4)
    * Add Users table
    
    * Add cloudside plugin to access !Ref and !GetAtt locally
    
    * Add user to DynamoDB

--- users-service/src/handlers/createUser.js
+++ users-service/src/handlers/createUser.js
@@ -7,15 +7,35 @@
 // A randomly generated amount of starting cash should be added (there should be a reasonable min/max)
 // A randomly generated number of starting stocks should be added (there should be a reasonable min/max)
 
+// Modules
+import { DynamoDB } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+
 import { commonMiddleware } from '../lib/commonMiddleware';
 import { successResponse } from '../lib/successResponse';
 
+const dynamoDB = new DynamoDB.DocumentClient();
+
 async function createUser(event, context) {
+  console.log(process.env.USERS_TABLE_NAME);
   try {
+    const now = new Date();
+
+    const user = {
+      id: uuid(),
+      created: now.toISOString(),
+    };
+
+    const params = {
+      TableName: process.env.USERS_TABLE_NAME,
+      Item: user,
+    };
+
+    await dynamoDB.put(params).promise();
 
     return successResponse({ message: 'User created!' });
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     throw error;
   }
 }
